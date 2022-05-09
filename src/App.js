961e6a64da7def242bed3d3802c970ad4d63e850
NgoDinhCanh <77870743+NgoDinhCanh@users.noreply.github.com>
2022-05-09 22:04:29 +0700

    Router_layout_finish

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { Fragment } from 'react';
+// Fragment the chua ko tao ra the tren dom
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '~/pages/Home';
-import Following from '~/pages/Following';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+
 function App() {
     return (
         <Router>
             <div className="App">
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/Following" element={<Following />} />
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
+                        if (route.layout) Layout = route.layout;
+                        else if (route.layout === null) Layout = Fragment;
+                        else Layout = DefaultLayout;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
                 </Routes>
             </div>
         </Router>
